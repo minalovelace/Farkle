fd86af8cf244f43441d07509da3214c611e5262a
minalovelace <someone@minalovelace.de>
2019-02-17 11:07:01 +0100

    Initial commit

---
+++ src/main/test/MainTest.java
@@ -0,0 +1,13 @@
+package main.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
